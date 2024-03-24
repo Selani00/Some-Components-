04f7371675dadbb224d6c8d00d7d591a5417ab89
Selani00 <pkselani00@gmail.com>
2024-03-24 20:25:57 +0530

    Pop up model created

---
+++ Front End/src/App.jsx
@@ -0,0 +1,26 @@
+import { Fragment, useState } from "react";
+import "./App.css";
+import Model from "./Components/model";
+
+function App() {
+  const [showModel, setShowModel] = useState(false);
+  return (
+    <Fragment>
+      <div className="p-10 text-center">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5" onClick={()=>setShowModel(true)}>
+          Text Model
+        </button>
+        <button className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5">
+          Video Model
+        </button>
+        <button className="mt-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5">
+          Input Model
+        </button>
+      </div>
+      <Model isVisible={showModel} onClose={()=>setShowModel(false)}/>
+    </Fragment>
+  );
+}
+
+export default App;
