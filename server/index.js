818983f487f923d611e6a8bfa07d7b6c701f3d5f
Selani00 <pkselani00@gmail.com>
2024-03-24 22:37:14 +0530

    backend post function

--- server/index.js
+++ server/index.js
@@ -9,17 +9,42 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
-// 'mongodb+srv://pkselani00:HO6AnLMPiB7C20dM@crud-table.sys6a7y.mongodb.net/CRUD-table?retryWrites=true&w=majority&appName=crud-table'
+const schemaData = mongoose.Schema({
+    name: String,
+    email: String,
+    mobile : Number,
+},{timestamps: true});
 
-mongoose.connect(process.env.MONGO)
+
+const userModel= mongoose.model('user', schemaData);
+
+
+// read
+app.get("/", async (req, res) => {
+    const data = await userModel.find({});
+    res.json({success: true, data:data});
+});
+
+// create / save data 
+app.post("/create",async (req,res)=>{
+    console.log(req.body);
+    const data = new userModel(req.body);
+    await data.save();
+    res.send({success: true, message : "Data saved"});
+
+})
+
+
+
+// 'mongodb+srv://pkselani00:HO6AnLMPiB7C20dM@crud-table.sys6a7y.mongodb.net/CRUD-table?retryWrites=true&w=majority&appName=crud-table'
+mongoose.connect('mongodb+srv://pkselani00:HO6AnLMPiB7C20dM@crud-table.sys6a7y.mongodb.net/CRUD-table?retryWrites=true&w=majority&appName=crud-table')
     .then(() => console.log('Database connected'))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-    res.json('Hello World from server');
-});
-
 app.listen(PORT, () => console.log("server is running"));
+
+
